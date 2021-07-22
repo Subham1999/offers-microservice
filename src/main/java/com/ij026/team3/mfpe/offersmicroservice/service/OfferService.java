bc1b7f4df7c6bac46b41957b680efe12e2662b5b
Subham1999 <subhamsantra2016@gmail.com>
2021-07-22 18:15:37 +0530

    limit logic changed

--- src/main/java/com/ij026/team3/mfpe/offersmicroservice/service/OfferService.java
+++ src/main/java/com/ij026/team3/mfpe/offersmicroservice/service/OfferService.java
@@ -134,8 +134,9 @@ public class OfferService implements GenericOfferService {
 		if (n > 0) {
 			// reverse sort on likes {max to min}
 			List<Offer> collect = offerRepository.findAll().stream().filter(predicate)
-					.sorted((o1, o2) -> o2.getLikes().size() - o1.getLikes().size()).collect(Collectors.toList());
-			return (n > collect.size()) ? List.of() : collect.subList(0, n);
+					.sorted((o1, o2) -> o2.getLikes().size() - o1.getLikes().size())
+					.collect(Collectors.toList());
+			return collect.subList(0, n);
 		} else {
 			return List.of();
 		}
